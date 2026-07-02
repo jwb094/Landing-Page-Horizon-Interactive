231737385bb828733967d4e3286440b7da0e3615
Josh Bello <Jb230913@live.com>
2026-07-02 14:21:16 +0100

    update - content for Content_Summary.jsx && (ContentSummary - component) change content from static to dummy data

--- src/components/content_Summary.jsx
+++ src/components/content_Summary.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../assets/styles/c-content-summary.css'
+import { ContentSummaryContent } from '../data/Content_Summary';
 function Content_Summary(props) {
     return (
         <section data-template="content-summary" data-view='default' className='c-content-summary'>
@@ -8,15 +9,15 @@ function Content_Summary(props) {
                     <div className="col-sm-12 col-md-6">
                         <div className="c-content-summary-content | d-flex flex-column">
                             <div className="c-content-summary-content__thin_red_line"></div>
-                            <h2>Title</h2>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur cumque doloremque, voluptate ut doloribus architecto quis quas mollitia laudantium quaerat, impedit, itaque neque dolore. Commodi blanditiis error laboriosam atque veniam?</p>
-                            <a className='d-flex' href="#">Learn More <i class="bi bi-chevron-right"></i></a>
+                            <h2>{ContentSummaryContent.title}</h2>
+                            <p>{ContentSummaryContent.content}</p>
+                            <a className='d-flex' href={ContentSummaryContent.cta.url}>{ContentSummaryContent.cta.label} <i class="bi bi-chevron-right"></i></a>
                         </div>
                     </div>
                     <div className="col-sm-12 col-md-6">
                         <div className="c-content-summary-media">
 
-                            <img src="https://placehold.co/600x400" className='w-100' alt="" />
+                            <img src={ContentSummaryContent.content_summary_image} className='w-100' alt="" />
 
                         </div>
                     </div>
---
+++ src/data/Content_Summary.jsx
@@ -0,0 +1,12 @@
+export const ContentSummaryContent = {
+    id: 1,
+    title: "Learn at Your Own Pace",
+    content:
+        "Access structured training courses anytime, anywhere. Learn web development, UI/UX, and backend skills at a speed that suits your schedule.",
+    cta: {
+        label: "Start Learning",
+        url: "/courses"
+    },
+    content_summary_image:
+        "https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?q=80&w=800&auto=format&fit=crop"
+};
