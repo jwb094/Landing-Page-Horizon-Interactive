8356f034c2deaf9e5fb7b96f6966e0f14f2aff19
Josh Bello <Jb230913@live.com>
2026-07-02 14:00:01 +0100

    updatev- switch from lorem static content to dummy content from data/ DIR

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-function HeroCard(props) {
+function HeroCard({ content }) {
+
     return (
         //d-none d-sm-block d-md-none d-md-block
         <div className="col-md-4">
-            <div className='c-img-hero-card | d-none d-lg-block d-xl-block'>
-                <div>Icon</div>
-                <h3 className='c-img-hero-card__title'>title</h3>
-                <p className='c-img-hero-card__content' >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis, nulla.</p>
+            <div className="c-img-hero-card |   d-none d-sm-block d-md-none d-lg-flex  d-xl-flex  | d-flex flex-column" key={content.id}>
+                {content.icon}
+                <div className="c-img-hero-card__thin_red_line"></div>
+                <h3 className='c-img-hero-card__title'>  {content.title}</h3>
+                <p className='c-img-hero-card__content' >  {content.description}</p>
             </div>
-        </div>
+        </div >
     );
 }
 
