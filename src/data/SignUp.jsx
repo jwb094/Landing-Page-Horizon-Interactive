d6d9daf878e682c0660b859da0fa6f93d7d61628
Josh Bello <Jb230913@live.com>
2026-07-02 21:34:48 +0100

    add - dumy content for Sign Up component content

---
+++ src/data/SignUp.jsx
@@ -0,0 +1,8 @@
+export const SignUpContent = {
+    tagline: "Ready to Get Started?",
+
+    heading: "Invest in Your Future with Industry-Ready Digital Skills",
+
+    description:
+        "Join thousands of learners developing in-demand skills in web development, UI/UX design, JavaScript, and software engineering. Create real-world projects, learn at your own pace, and take the next step in your career."
+};
