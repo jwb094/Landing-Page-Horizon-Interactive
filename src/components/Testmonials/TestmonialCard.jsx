9443040206c7f02517261aa0eceed4df6be1cd73
Josh Bello <Jb230913@live.com>
2026-07-04 11:07:07 +0100

    update - UI component with props data

--- src/components/Testmonials/TestmonialCard.jsx
+++ src/components/Testmonials/TestmonialCard.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
 function TestmonialCard({ content }) {
-    console.log(content);
+
+    const rating = [];
+    {
+        for (let index = 0; index < content.rating; index++) {
+
+            rating.push(<i className="bi bi-star-fill text-warning"></i>)
+
+        }
+    }
     return (
-        <div className='c-testmonial-card  | d-flex flex-column  text-center'>
+        <div className='c-testmonial-card shadow | d-flex flex-column  text-center' key={content.id}>
             <div className='c-testmonial-card-media'>
-                <img src="https://placehold.co/128x128" alt="" />
+                <img className='rounded-circle' src={content.image} alt={`profile pic of ${content.name}`} />
             </div>
             <div className='c-testmonial-card-content'>
-                {content.id}
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, laborum!</p>
-                <small className='rating'>Rating</small>
+                <p>{content.content}</p>
+                <small className='rating'>
+
+                    {rating}
+
+                </small>
                 <div className='personal | d-flex flex-column'>
-                    <h5>FirstN LastN</h5>
-                    <h6>Occupation</h6>
+                    <h5>{content.name}</h5>
+                    <h6>{content.occupation}</h6>
                 </div>
             </div>
         </div>
